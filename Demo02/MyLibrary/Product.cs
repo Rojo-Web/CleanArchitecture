ac182c3d40e8661474b5b4c73a317c1987b7776f
Rojo-Web <santifa2004@gmail.com>
2024-10-13 23:41:52 -0500

    avance en el proyecto demo02

---
+++ Demo02/MyLibrary/Product.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace MyLibrary;
+
+    public class Product(int id, string name, decimal unitPrice, int unitsInStock)
+    {
+        //Propiedades
+        public int Id => Id;
+        public string Name => Name;
+        public decimal UnitPrice => UnitPrice;
+        public decimal unitsInStock => unitsInStock;
+        public bool Discontinued { get; set; }
+        public void SetDiscontinued() => Discontinued = true;
+        public bool GetStatus() => Discontinued;
+    }
+
