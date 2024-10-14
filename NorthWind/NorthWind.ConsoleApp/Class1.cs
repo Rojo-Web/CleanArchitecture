e135124f66379584ddf6b6ab2f2c6fe67aa86ae0
Rojo-Web <santifa2004@gmail.com>
2024-10-14 09:49:09 -0500

    NorthWind, avance sesion 4 a

---
+++ NorthWind/NorthWind.ConsoleApp/Class1.cs
@@ -0,0 +1,20 @@
+﻿
+
+using NorthWind.ConsoleApp.Services;
+using NorthWind.Entities.Interfaces;
+using NorthWind.Writers;
+
+IUserActionWriter Writer = new FileWriter();
+
+AppLogger Logger = new AppLogger(Writer);
+Logger.WriteLog("Aplication Started");
+
+ProductService Service = new ProductService(Writer);
+Service.Add("Demo","Azucar refinada");
+
+/*
+ *AppLoger y los Writers: Resposabilidad unica
+ *AppLoger: Abierto pero cerrado 
+ *AppLoger: Inversione de dependencias. Los modulos
+ *de alto nivel son independientes de los detalles de implementacion.
+ */
---
+++ NorthWind/NorthWind.ConsoleApp/Services/ProductService.cs
@@ -0,0 +1,15 @@
+﻿using NorthWind.Entities.Interfaces;
+using NorthWind.Entities.ValueObjects;
+
+
+namespace NorthWind.ConsoleApp.Services
+{
+    internal class ProductService(IUserActionWriter writer)
+    {
+        public void Add(string user, string prodcutName)
+        {
+            UserActions Action = new UserActions(user, $"Created: {prodcutName}");
+            writer.Write(Action);
+        }
+    }
+}
